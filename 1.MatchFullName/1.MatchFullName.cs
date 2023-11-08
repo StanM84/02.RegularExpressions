9582e8586b98d1eb6d0659f507f3b0b94ac7200b
StanM84 <stan1984@abv.bg>
2023-11-08 13:29:07 +0200

    Add project files.

---
+++ 1.MatchFullName/1.MatchFullName.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+";
+
+Regex regex =  new Regex(pattern);
+
+string inputs = Console.ReadLine();
+
+MatchCollection matches = regex.Matches(inputs);
+
+Console.WriteLine(string.Join(" ", matches));
